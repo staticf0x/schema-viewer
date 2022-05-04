ef3d8093a1084620bb6648784f4562056e8fe98c
staticf0x <44530786+staticf0x@users.noreply.github.com>
2022-05-04 11:35:36 +0200

    Handle allOf, display paths

---
+++ schema_viewer/helpers.py
@@ -0,0 +1,14 @@
+import re
+
+
+def extract_default(s):
+    r = re.findall(r"Default value is (\w+)\b", s)
+
+    if r:
+        return r[0]
+
+    return ""
+
+
+def format_description(d):
+    return re.sub(r"https://([\w\.\/\-\#\d\?\=\+]+)", r'<a href="https://\1">https://\1</a>', d)
